3b8c18495f5b8e345525127b20793564909b761f
guosai <501807312@qq.com>
2025-07-16 17:54:55 +0800

    1

--- src/Contract/HttpClientInterface.php
+++ src/Contract/HttpClientInterface.php
@@ -6,13 +6,13 @@ use Webguosai\HttpClient\Response;
 
 /**
  * Http客户端
- * @method Response get(string $url, array $query, string|array $headers)
- * @method Response post(string $url, string|array $data, string|array $headers)
- * @method Response put(string $url, string|array $data, string|array $headers)
- * @method Response patch(string $url, string|array $data, string|array $headers)
- * @method Response delete(string $url, string|array $data, string|array $headers)
- * @method Response head(string $url, string|array $data, string|array $headers)
- * @method Response options(string $url, string|array $data, string|array $headers)
+ * @method Response get(string $url, array $query = [], string|array $headers = [])
+ * @method Response post(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response put(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response patch(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response delete(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response head(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response options(string $url, string|array $data = [], string|array $headers = [])
  */
 interface HttpClientInterface
 {
